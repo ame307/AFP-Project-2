bb122a7e50cd0c4ddf32ccf897c248527c28c94c
Lázár Dávid <tesa307@gmail.com>
2019-10-22 15:20:16 +0200

    create application

---
+++ Application/Demo/mern/src/components/cars-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CarsList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Car List Component</p>
+            </div>
+        )
+    }
+}
