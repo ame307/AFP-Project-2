4e11027591339a816444b1899ea3c90de43e368f
Lázár Dávid <tesa307@gmail.com>
2019-10-22 15:24:07 +0200

    Revert "create application"
    This reverts commit bb122a7e50cd0c4ddf32ccf897c248527c28c94c.

--- Application/Demo/mern/src/App.js
+++ Application/Demo/mern/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
-
-import Navbar from "./components/navbar.component"
-import CarsList from "./components/cars-list.component";
-import EditCar from "./components/edit-car.component";
-import CreateCar from "./components/create-car.component";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Navbar />
-        <br/>
-        <Route path="/" exact component={CarsList} />
-        <Route path="/edit/:id" component={EditCar} />
-        <Route path="/create" component={CreateCar} />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
