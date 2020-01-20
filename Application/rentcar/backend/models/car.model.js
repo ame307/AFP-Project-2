552cd30b5284cc605f79c8e12c9580289e6ed7bb
Alastair94 <szondiazazel@gmail.com>
2020-01-20 21:08:46 +0100

    Expand car-model
    Added "reserved" property to carSchema

--- Application/rentcar/backend/models/car.model.js
+++ Application/rentcar/backend/models/car.model.js
@@ -6,7 +6,8 @@ const carSchema = new Schema({
     brand: {type: String, required: true, trim: true, minlength: 3},
     model: {type: String, required: true, trim: true, minlength: 1},
     consumption: {type: Number, required: true, trim: true, minlength: 1},
-    plateNumber: {type: String, required: true, trim: true, unique: true}
+    plateNumber: {type: String, required: true, trim: true, unique: true},
+    reserved: {type: Boolean, required: true, trim: true}
 }, {
     timestamps: true,
 });
